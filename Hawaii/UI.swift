d87e34afe8778e9860ee760156469dbfb835a29a
Frederick C. Lee <amourinetech@gmail.com>
2018-02-24 14:47:45 -0800

    Added UISwift.

---
+++ Hawaii/UI.swift
@@ -0,0 +1,34 @@
+//
+//  UI.swift
+//  Hawaii
+//
+//  Created by Frederick C. Lee on 2/24/18.
+//  Copyright © 2018 Amourine Technologies. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension MainViewController {
+    
+    func setupUI() {
+ 
+//        imageBackground.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        imageBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        imageBackground.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//         imageBackground.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        
+        
+//        imageBackground.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+//                              bottom: view.safeAreaLayoutGuide.bottomAnchor,
+//                              left: view.safeAreaLayoutGuide.leftAnchor,
+//                              right: view.safeAreaLayoutGuide.rightAnchor,
+//                              centerYAnchor: nil,
+//                              centerXAnchor: nil,
+//                              paddingTop: -40,
+//                              paddingLeft: 0,
+//                              paddingBottom: 10,
+//                              paddingRight: 10, width: 0, height: 0)
+    }
+}
