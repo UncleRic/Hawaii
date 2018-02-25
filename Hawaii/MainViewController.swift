2ae2089510239f0df43d4ef0763877ba328c3d74
Frederick C. Lee <amourinetech@gmail.com>
2018-02-25 11:32:42 -0800

    Got Hawaii Weather.

--- Hawaii/MainViewController.swift
+++ Hawaii/MainViewController.swift
@@ -10,14 +10,44 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var portraitBackgroundImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            portraitBackgroundImageView.alpha = 0.0
+            print("Landscape")
+        } else {
+            portraitBackgroundImageView.alpha = 1.0
+                    portraitBackgroundImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                          bottom: nil,
+                                          left: view.safeAreaLayoutGuide.leftAnchor,
+                                          right: view.safeAreaLayoutGuide.rightAnchor,
+                                          centerYAnchor: nil,
+                                          centerXAnchor: nil,
+                                          paddingTop: -40,
+                                          paddingLeft: 0,
+                                          paddingBottom: 10,
+                                          paddingRight: 10, width: 0, height: 200)
+
+            print("Portrait")
+        }
+    }
+    
+    
+    @IBAction func weatherAction() {
+        let weatherReport = WeatherReport()
+        weatherReport.getWeatherData(sender: self)
     }
     
 }
--- Hawaii/WeatherService.swift
+++ Hawaii/WeatherService.swift
@@ -8,12 +8,76 @@
 
 import UIKit
 
-struct WeatherResource {
-    let url:URL
-    let parse:(Data) -> Data?
-}
-
-final class WeatherService {
+class WeatherReport {
+    public struct Weather: Codable {
+        let id: Int
+        let main: String
+        let description: String
+        let icon: String
+    }
+    
+    public struct Main: Codable {
+        let temp: Float
+        let pressure: Float
+        let humidity: Float
+        let temp_min: Float
+        let temp_max: Float
+    }
+    
+    public struct Wind: Codable {
+        let speed: Float
+        let deg: Float
+    }
+    
+    public struct Clouds: Codable {
+        let all: Int
+    }
+    
+    public struct Sys: Codable {
+        let type: Int
+        let id: Int
+        let message: Double
+        let country: String
+        let sunrise: Double
+        let sunset: Double
+    }
+    
+    struct DataListModel: Codable {
+        let coord : [String:Float]
+        let weather : [Weather]
+        let base: String
+        let main: Main
+        let visibility: Double
+        let wind: Wind
+        let clouds: [String:Int]
+        let dt: Double
+        let sys: Sys
+        let id: Int
+        let name: String
+        let cod: Int
+    }
+    
+    func disseminateJSON(data: Data) -> DataListModel? {
+        var weatherStuff:DataListModel?
+        do {
+            weatherStuff = try JSONDecoder().decode(DataListModel.self, from: data)
+        } catch let error as NSError {
+            let title = "JSON Dissemination Error"
+            let alert = UIAlertController(title: title, message: error.debugDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .`default`, handler: nil))
+            //   self.present(alert, animated: true, completion: nil)
+        }
+        return weatherStuff
+    }
+    
+    // ===================================================================================================
+    
+    struct WeatherResource {
+        let url:URL
+        let parse:(Data) -> Data?
+    }
+    
+    
     final func load(resource: WeatherResource, completion: @escaping (Any?) -> Void) {
         URLSession.shared.dataTask(with: resource.url) { (data, _, error) in
             if let error = error {
@@ -25,8 +89,41 @@ final class WeatherService {
             }
             }.resume()
     }
+    
+    
+    // -----------------------------------------------------------------------------------------------------
+    
+    final func getWeatherData(sender: MainViewController)  {
+        // Hawaii ID = '5856194'
+        let weatherURIString = "https://api.openweathermap.org/data/2.5/weather?id=5856194&APPID=44d0f6b3ed7092adb89091cbed5372b4"
+        let url = URL(string:weatherURIString)
+        
+        let weatherResource = WeatherResource(url: url!) {(data) -> Data? in
+            return data
+        }
+        self.load(resource: weatherResource) {result in
+            DispatchQueue.main.async(execute: {
+                if let errorDescription = result as? String {
+                    print(errorDescription)
+                } else if let jsonData = result as? Data {
+                    let weatherData = self.disseminateJSON(data: jsonData)
+                    if let name = weatherData?.name,
+                        let temp = weatherData?.main.temp,
+                        let weatherDesc = weatherData?.weather[0].description {
+                        let kelvinConversion = Float(273.15)
+                        let celsius = NSNumber(value:Float(temp) - kelvinConversion)
+                        let formatter = NumberFormatter()
+                        formatter.numberStyle = .decimal
+                        formatter.maximumFractionDigits = 2
+                        if let celsiusString = formatter.string(from: celsius) {
+                            //    self.cityInfo = (name, celsiusString, weatherDesc)
+                        }
+                    }
+                }
+            })
+        }
+    }
 }
-
 
 
 
