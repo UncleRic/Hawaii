e28d463a083a730e0b623d829aa8808a7962c3d8
Frederick C. Lee <amourinetech@gmail.com>
2018-03-05 16:07:14 -0800

    Add Oahu images.

--- Hawaii/Island View Controllers/OahuViewController.swift
+++ Hawaii/Island View Controllers/OahuViewController.swift
@@ -13,12 +13,14 @@ class OahuViewController: UIViewController, BackgroundDisplay {
     var portraitBackgroundImage:UIImage?
     var landscapeBackgroundImage: UIImage?
     var backgroundImageView:UIImageView?
+    var backgroundScrollView = UIScrollView(frame: CGRect.zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
         self.view.gestureRecognizers = [tapGesture]
-        setupLandscapeBackground()
+        loadImages()
+        setupPortraitBackground()
         setupToolBar()
     }
     
@@ -26,30 +28,51 @@ class OahuViewController: UIViewController, BackgroundDisplay {
         super.viewWillTransition(to: size, with: coordinator)
         if UIDevice.current.orientation.isLandscape {
             removeVestigialViews()
+            backgroundImageView?.removeFromSuperview()
+            setupLandscapeBackground()
+        } else {
+            backgroundScrollView.removeFromSuperview()
+            setupPortraitBackground()
         }
     }
     
     // -----------------------------------------------------------------------------------------------------
     
-    func setupLandscapeBackground() {
-        let image = UIImage(named:"Honolulu")
-        let imageView = UIImageView(image: image)
-        imageView.contentMode = .scaleAspectFill
+    func setupPortraitBackground() {
         view.backgroundColor = UIColor.white
-        
-        
-        view.addSubview(imageView)
-        imageView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-                         bottom: view.safeAreaLayoutGuide.bottomAnchor,
-                         left: view.safeAreaLayoutGuide.leftAnchor,
-                         right: view.safeAreaLayoutGuide.rightAnchor,
-                         centerYAnchor: nil,
-                         centerXAnchor: nil,
-                         paddingTop: 0,
-                         paddingLeft: 0,
-                         paddingBottom: 10,
-                         paddingRight: 0, width: 0, height: 0)
-        
+        if nil == backgroundImageView {
+            backgroundImageView = UIImageView(image:UIImage(named:"Honolulu"))
+            backgroundImageView?.contentMode = .scaleAspectFill
+        }
+        view.addSubview(backgroundImageView!)
+        backgroundImageView?.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                    bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                    left: view.safeAreaLayoutGuide.leftAnchor,
+                                    right: view.safeAreaLayoutGuide.rightAnchor,
+                                    centerYAnchor: nil,
+                                    centerXAnchor: nil,
+                                    paddingTop: 0,
+                                    paddingLeft: 0,
+                                    paddingBottom: 10,
+                                    paddingRight: 0, width: 0, height: 0)
+    }
+    
+    
+    // -----------------------------------------------------------------------------------------------------
+    
+    func setupLandscapeBackground() {
+        view.backgroundColor = UIColor.black
+        view.addSubview(backgroundScrollView)
+        backgroundScrollView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                    bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                    left: view.safeAreaLayoutGuide.leftAnchor,
+                                    right: view.safeAreaLayoutGuide.rightAnchor,
+                                    centerYAnchor: nil,
+                                    centerXAnchor: nil,
+                                    paddingTop: 0,
+                                    paddingLeft: 0,
+                                    paddingBottom: 10,
+                                    paddingRight: 0, width: 0, height: 0)
     }
     
     // -----------------------------------------------------------------------------------------------------
@@ -64,6 +87,46 @@ class OahuViewController: UIViewController, BackgroundDisplay {
         }
         if let overlay = self.view.viewWithTag(IslandAssets.overlayView.rawValue) {
             overlay.removeFromSuperview()
+        }
+    }
+    
+    fileprivate func loadImages() {
+        let imageView1 = UIImageView(image: UIImage(named: "Oahu1"))
+        let imageView2 = UIImageView(image: UIImage(named: "Oahu2"))
+        let imageView3 = UIImageView(image: UIImage(named: "Oahu3"))
+        let imageView4 = UIImageView(image: UIImage(named: "Oahu4"))
+        let imageView5 = UIImageView(image: UIImage(named: "Oahu5"))
+        let imageView6 = UIImageView(image: UIImage(named: "Oahu6"))
+        let imageView7 = UIImageView(image: UIImage(named: "Oahu7"))
+        let imageView8 = UIImageView(image: UIImage(named: "Oahu8"))
+        let imageView9 = UIImageView(image: UIImage(named: "Oahu9"))
+        let imageView10 = UIImageView(image: UIImage(named: "Oahu10"))
+        let imageView11 = UIImageView(image: UIImage(named: "Oahu11"))
+        let imageView12 = UIImageView(image: UIImage(named: "Oahu12"))
+        let imageView13 = UIImageView(image: UIImage(named: "Oahu13"))
+        let imageView14 = UIImageView(image: UIImage(named: "Oahu14 (kualoa)"))
+        let imageView15 = UIImageView(image: UIImage(named: "Oahu15 (kualoa-ranch)"))
+        let imageView16 = UIImageView(image: UIImage(named: "Oahu16 (Kailua)"))
+        let imageView17 = UIImageView(image: UIImage(named: "Oahu17"))
+        
+        let photos = [imageView9, imageView11, imageView2, imageView4, imageView6, imageView7, imageView10,  imageView13, imageView17, imageView1, imageView3, imageView5, imageView8, imageView12, imageView16, imageView14, imageView15]
+        var xPosition:CGFloat = 0.0
+        
+        backgroundScrollView.isPagingEnabled = true
+        
+        var scrollViewContentWidth:CGFloat = 0
+        let space:CGFloat = 10.0
+        
+        for photo in photos {
+            photo.frame.size.width = UIScreen.main.bounds.height
+            photo.frame.size.height = UIScreen.main.bounds.width
+            //photo.center = self.view.center
+            photo.contentMode = .scaleAspectFit
+            photo.frame.origin.x = xPosition
+            backgroundScrollView.addSubview(photo)
+            xPosition += photo.frame.size.width + space
+            scrollViewContentWidth += photo.frame.size.width + space
+            backgroundScrollView.contentSize = CGSize(width: scrollViewContentWidth, height: photo.frame.size.height)
         }
     }
     
